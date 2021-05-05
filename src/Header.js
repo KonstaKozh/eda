437d469f835c28a94f850980757b6e658c1793f9
Konstakozh <konstakozh@yandex.ru>
2021-05-05 10:50:49 +0400

    add Header.js

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const Header = () => {
+    return (
+        <img src="../public/koolinar-logo.svg" alt="logo-eda"/>
+
+    )
+}
