2363ba6ff788f952f72e6c15577ba6a106dd6018
Konstakozh <konstakozh@yandex.ru>
2021-05-11 23:37:32 +0400

    edit RecipeList RecipeItem

--- src/RecipeList/RecipeItem.js
+++ src/RecipeList/RecipeItem.js
@@ -12,7 +12,7 @@ export const RecipeItem = ({item}) => {
 
     return (
         <li className='recipe-item-container'>
-            <h2 className='recipe-title'>{formattedItem.title}</h2>
+            <h2 className='recipe-title'><a href="#">{formattedItem.title}</a></h2>
             <img className='recipe-img' src={formattedItem.imgSrc} alt="imgItem"/>
             <p className='recipe-description'>{formattedItem.description}</p>
             <span className="recipe-author">{formattedItem.author}</span>
--- src/RecipeList/RecipeList.js
+++ src/RecipeList/RecipeList.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {RecipeItem} from "./RecipeItem"
+import './RecipeList.scss'
 
 export const RecipeList = () => {
-    const i = {title: 'title', imgSrc: 'imgSrc1', description: 'description', author: 'author', date: 'date'}
-    const i2 = {title: 'title2', imgSrc: 'imgSrc2', description: 'description2', author: 'author2', date: 'date2'}
-    const i3 = {title: 'title3', imgSrc: 'imgSrc3', description: 'description3', author: 'author3', date: 'date3'}
+    const i = {title: 'Блинчики с начинкой', imgSrc: '../img/img1.jpg', description: 'Блины с начинкой из субпродуктов и риса', author: 'Linda82 (Marioka82)', date: '12 февраля 2015, 13:18'}
+    const i2 = {title: 'Куббе (kubbeh)', imgSrc: '../img/img2.jpg', description: 'Cуп с клёцками из булгура с мясной начинкой', author: 'chef', date: '06 июля 2011, 05:34'}
+    const i3 = {title: 'Утиное филе', imgSrc: '../img/img3.jpg', description: 'Утиное филе с лавандово-смородиновым сорбетом', author: 'chef', date: '03 октября 2012, 16:16'}
 
     const recipes = [i, i2, i3]
 
     return (
-        <div>
+        <div className='recipe-container'>
             <ul>
                 {recipes.map(item => (<RecipeItem item={item} />))}
-
             </ul>
         </div>
     )
